4d2222a8a7d6516624413f560b019dd7ca996fbe
mitburr <mitburr@gmail.com>
2024-03-30 17:54:31 -0500

    decisions to model

--- src/models/players/model.py
+++ src/models/players/model.py
@@ -21,5 +21,24 @@ class Player:
         if self.cards:
             card = self.cards.pop()
             print(f"{self.name} has lost an influence: {card.card_style}.")
+"""
+import random     
+import time   
+from rich.prompt import Prompt, Confirm
+
+def decision(text, choices, player):
+    if player.is_bot:
+        print(f"{player.name} is thinking...\n")
+        time.sleep(1)
+        if choices == ["y", "n"]:
+            return random.choice([True, False])
+        return random.choice(choices)
+    else:
+        if choices == ["y","n"]:
+            return Confirm.ask(f"{text}")
+        else:
+            return Prompt.ask(text, choices=choices)
+"""
+
 
     
